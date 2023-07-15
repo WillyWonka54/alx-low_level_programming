b86f07904f8a0097b9b672dd82b32f0b02dc5335
WillyWonka54 <walid.ahmed9411@gmail.com>
2023-07-14 19:23:53 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,10 @@ int main(void)
 	{
 		putchar(digit);
 	/* after 9 we jump till 96; */
-	if (digit == 57)
-		digit += 39;
+		if (digit == 57)
+		{
+			digit += 39;
+		}
 		digit++;
 	}
 	putchar(\n');
