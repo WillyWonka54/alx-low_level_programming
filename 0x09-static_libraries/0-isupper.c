6bd5e34cb79b1d2a012831dfcaeca355c3033772
WillyWonka54 <walid.ahmed9411@gmail.com>
2023-08-09 14:41:09 -0700

    Done

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -4,7 +4,7 @@
  * _isupper - checks if the character is uppercase
  * @c: is the int used for the argument of the function.
  * Return: 1 if uppercase character 0 if not
- */
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
